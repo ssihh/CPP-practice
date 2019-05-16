719dde94c893da9b20fe3c2bc96e4199d03720a1
si hyeon <jnsh0308@naver.com>
2019-05-16 22:49:21 +0900

    시작

---
+++ 연습0514/1.4.cpp
@@ -0,0 +1,11 @@
+// 첫 번째 프로그램
+
+#include <iostream>		//헤더파일 입출력에 필요한 클래스들이 정의
+using namespace std;	//이름 공간 설정
+
+int main(void)
+{
+	cout << "Hello World!" << endl;		//cout, cin / endl=\n
+	return 0;		//0반환 정상종료
+
+}
---
+++ 연습0514/test.cpp
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//
+//
+//void main()
+//{
+//	printf("korea\n");
+//	printf("korea\n");
+//
+//}
+//
